0050ef7d6eafe08678c6b4e95a0fac19e94288ad
dmitry tkachenko <dmitry.tkachenko@rokolabs.com>
2017-08-31 02:00:18 +0400

    Minor fixes.

---
+++ PoeApiNet.Entities/PropertyValue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PoeApiNet.Enums;
+
+namespace PoeApiNet.Entities
+{
+	public class PropertyValue
+	{
+		public string Value { get; set; }
+		public int ValueTypes { get; set; }
+		public Dimension Dimension { get; set; }
+	}
+}
